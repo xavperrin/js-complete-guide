cf05c26286ef93c69b402ecb1b11340fd00566a8
xavperrin <xl.perrin@gmail.com>
2023-01-18 12:53:53 +0100

    about block scope & global scope variables

---
+++ behind-scene/01-starting-setup/app.js
@@ -0,0 +1,20 @@
+let name ='Max';
+
+if(name==='Max'){
+var hobbies=['Sport', 'cooking'];
+console.log(hobbies);
+
+}
+
+
+function greet(){
+    let age=30;
+    let name='Manuel';
+    console.log(name, age, hobbies);
+
+
+}
+
+console.log(name, hobbies);
+
+greet();
